a5c4da84c756de0fdef48645bbbff24810a41153
Rixio Barrios <55994508+rixiobarrios@users.noreply.github.com>
2024-06-11 16:24:39 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+# Modile 1
+# What is Python
+# Execute the following code and see what happens. Feel free to change it and run it as many times as you want
+
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
